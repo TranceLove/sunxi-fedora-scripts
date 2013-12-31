e9055e1365fd1ce814d83a79f3d3187a8918c44e
Raymond Lai <airwave209gt@gmail.com>
2013-12-31 17:24:09 +0800

    Trim down script to become A10 tab/Meep1 specific

--- select-board.sh
+++ select-board.sh
@@ -13,56 +13,7 @@ UBOOT_DEV="$(df $CANON | tail -n 1 | awk '{print $1}')"
 
 BOARDS=()
 BOARDS+=(a10_mid_1gb         "A10 tablet sold under various names (whitelabel)")
-BOARDS+=(a13_mid             "A13 tablet sold under various names (whitelabel)")
-BOARDS+=(a10s-olinuxino-m          "A10s-OLinuXino-MICRO")
-BOARDS+=(a10s-olinuxino-m-lcd7     "A10s-OLinuXino-MICRO with 7\" lcd module")
-BOARDS+=(a10s-olinuxino-m-lcd10    "A10s-OLinuXino-MICRO with 10\" lcd module")
-BOARDS+=(a13-olinuxino             "A13-OLinuXino")
-BOARDS+=(a13-olinuxino-lcd7        "A13-OLinuXino with 7\" lcd module")
-BOARDS+=(a13-olinuxino-lcd10       "A13-OLinuXino with 10\" lcd module")
-BOARDS+=(a13-olinuxinom            "A13-OLinuXino-MICRO")
-BOARDS+=(a13-olinuxinom-lcd7       "A13-OLinuXino-MICRO with 7\" lcd module")
-BOARDS+=(a13-olinuxinom-lcd10      "A13-OLinuXino-MICRO with 10\" lcd module")
-BOARDS+=(a20-olinuxino_micro       "A20-OLinuXino-MICRO")
-BOARDS+=(a20-olinuxino_micro-lcd7  "A20-OLinuXino-MICRO with 7\" lcd module")
-BOARDS+=(a20-olinuxino_micro-lcd10 "A20-OLinuXino-MICRO with 10\" lcd module")
-BOARDS+=(auxtek-t003         "Auxtek T003 hdmi tv stick")
-BOARDS+=(auxtek-t004         "Auxtek T004 hdmi tv stick")
-BOARDS+=(ba10_tv_box         "BA10 TV Box")
-BOARDS+=(coby_mid7042        "Coby MID7042 tablet")
-BOARDS+=(coby_mid8042        "Coby MID8042 tablet")
-BOARDS+=(coby_mid9742        "Coby MID9742 tablet")
-BOARDS+=(cubieboard_512      "Cubieboard development board 512 MB RAM")
-BOARDS+=(cubieboard          "Cubieboard development board 1024 MB RAM")
-BOARDS+=(cubieboard2         "Cubieboard 2 (A20) development board")
-BOARDS+=(cubietruck          "Cubieboard Truck (A20) development board")
-BOARDS+=(dns_m82             "DNS AirTab M82 tablet")
-BOARDS+=(eoma68_a10          "EOMA68 A10 CPU card")
-BOARDS+=(gooseberry_a721     "Gooseberry development board")
-BOARDS+=(h6                  "H6 netbook")
-BOARDS+=(hackberry           "Hackberry development board")
-BOARDS+=(hyundai_a7hd        "Hyundai a7hd tablet")
-BOARDS+=(inet97f-ii          "iNet-97F Rev.2 (and clones) tablet")
-BOARDS+=(marsboard_a10       "Marsboard A10 development board")
-BOARDS+=(megafeis_a08        "Megafeis A08 hdmi stick")
-BOARDS+=(mele_a1000          "Mele a1000/a2000 512 MB RAM")
-BOARDS+=(mele_a1000g         "Mele a1000g/a2000g 1024 MB RAM")
-BOARDS+=(mele_a3700          "Mele a3700 (a1000g without sata)")
-BOARDS+=(mini-x              "Mini-X 512 MB RAM")
-BOARDS+=(mini-x-1gb          "Mini-X 1024 MB RAM")
-BOARDS+=(mini-x_a10s         "Mini-X with A10s soc")
-BOARDS+=(mk802               "mk802 (with female mini hdmi) 512 MB RAM")
-BOARDS+=(mk802-1gb           "mk802 (with female mini hdmi) 1024 MB RAM")
-BOARDS+=(mk802_a10s          "mk802 with A10s (s with a circle around it on the barcode label")
-BOARDS+=(mk802ii             "mk802ii (with male normal hdmi) 1024 MB RAM")
-BOARDS+=(pcduino             "pcDuino development board")
-BOARDS+=(pov_protab2_ips9    "Point of View ProTab 2 IPS 9\" tablet")
-BOARDS+=(pov_protab2_ips_3g  "Point of View ProTab 2 IPS tablet with 3g")
-BOARDS+=(r7-tv-dongle        "r7 hdmi tv stick")
-BOARDS+=(sanei_n90           "Sanei N90 tablet")
-BOARDS+=(uhost_u1a           "UHost U1A hdmi tv stick")
-BOARDS+=(wobo-i5             "Wobo i5 TV Box")
-BOARDS+=(xzpad700            "XZPAD700 7\" tablet")
+BOARDS+=(meep1               "Oregon Scientific Meeppad 1")
 
 if [ "$1" = "--help" -o -z "$DIALOG" -a -z "$BOARD" ]; then
     echo "Usage: \"$0 <board>\""
@@ -123,10 +74,6 @@ esac
 
 if [ -d $BOARDS_DIR/sun4i/$BOARD ]; then
     ARCH=sun4i
-elif [ -d $BOARDS_DIR/sun5i/$BOARD ]; then
-    ARCH=sun5i
-elif [ -d $BOARDS_DIR/sun7i/$BOARD ]; then
-    ARCH=sun7i
 else
     echo "Error cannot find board dir: $BOARDS_DIR/sun?i/$BOARD"
     exit 1
@@ -140,7 +87,6 @@ echo "Installing spl, u-boot and kernel for $BOARD$LCD onto $SDCARD_DEV ..."
 dd if="$BOARDS_DIR/$ARCH/$BOARD/u-boot-sunxi-with-spl.bin" of="$SDCARD_DEV" bs=1024 seek=8
 dd if="$BOARDS_DIR/uEnv-img.bin" of="$SDCARD_DEV" bs=1024 seek=544
 cp "$BOARDS_DIR/$ARCH/$BOARD/script$LCD.bin" "$UBOOT_MOUNT/script.bin"
-cp "$UBOOT_MOUNT/uImage.$ARCH" "$UBOOT_MOUNT/uImage"
 sync
 
 echo "Done"
