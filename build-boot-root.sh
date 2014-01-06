173f674919a72e7a6b33d70146bf2864a5870b8b
Raymond Lai <airwave209gt@gmail.com>
2014-01-06 16:15:31 +0800

    Put back the routines building u-boot and kernels for all other devices, as well as rootfs-resize service

--- build-boot-root.sh
+++ build-boot-root.sh
@@ -37,8 +37,10 @@
 # https://github.com/jwrdegoede/linux-sunxi.git
 
 KERNER_VER=3.4
-A10_BOARDS="A10_MID_1GB"
-A10_BOARD_FEXS="A10_MID_1GB meep1"
+A10_BOARDS="a10_mid_1gb ba10_tv_box coby_mid7042 coby_mid8042 coby_mid9742 cubieboard cubieboard_512 dns_m82 eoma68_a10 gooseberry_a721 h6 hackberry hyundai_a7hd inet97f-ii jesurun-q5 marsboard_a10 mele_a1000 mele_a1000g mele_a3700 mini-x mini-x-1gb mk802 mk802-1gb mk802ii pcduino pov_protab2_ips9 pov_protab2_ips_3g sanei_n90 uhost_u1a"
+A13_BOARDS="a13_mid a13-olinuxino xzpad700"
+A20_BOARDS="a20-olinuxino_micro cubieboard2 cubietruck eu3000"
+A10_BOARD_FEXS="a10_mid_1gb meep1 ba10_tv_box coby_mid7042 coby_mid8042 coby_mid9742 cubieboard cubieboard_512 dns_m82 eoma68_a10 gooseberry_a721 h6 hackberry hyundai_a7hd inet97f-ii jesurun-q5 marsboard_a10 mele_a1000 mele_a1000g mele_a3700 mini-x mini-x-1gb mk802 mk802-1gb mk802ii pcduino pov_protab2_ips9 pov_protab2_ips_3g sanei_n90 uhost_u1a"
 UBOOT_TAG=sunxi
 KERNEL_CONFIG_TAG=fedora-19-13102013-meep1
 KERNEL_TAG=sunxi-3.4
@@ -78,10 +80,24 @@ mkdir $DESTDIR/uboot/boards
 # Note the changing board configs always force a rebuild
 mkdir $DESTDIR/uboot/boards/sun4i
 for i in $A10_BOARDS; do
-    make -j10 CROSS_COMPILE=arm-linux-gnueabihf- O=$i ${i}_config
-    make -j10 CROSS_COMPILE=arm-linux-gnueabihf- O=$i
+    make -j8 CROSS_COMPILE=arm-linux-gnueabihf- O=$i ${i}_config
+    make -j8 CROSS_COMPILE=arm-linux-gnueabihf- O=$i
     mkdir $DESTDIR/uboot/boards/sun4i/$i
     cp $i/u-boot-sunxi-with-spl.bin $DESTDIR/uboot/boards/sun4i/$i
+done
+mkdir $DESTDIR/uboot/boards/sun5i
+for i in $A13_BOARDS; do
+    make -j8 CROSS_COMPILE=arm-linux-gnueabihf- O=$i ${i}_config
+    make -j8 CROSS_COMPILE=arm-linux-gnueabihf- O=$i
+    mkdir $DESTDIR/uboot/boards/sun5i/$i
+    cp $i/u-boot-sunxi-with-spl.bin $DESTDIR/uboot/boards/sun5i/$i
+done
+mkdir $DESTDIR/uboot/boards/sun7i
+for i in $A20_BOARDS; do
+    make -j8 CROSS_COMPILE=arm-linux-gnueabihf- O=$i ${i}_config
+    make -j8 CROSS_COMPILE=arm-linux-gnueabihf- O=$i
+    mkdir $DESTDIR/uboot/boards/sun7i/$i
+    cp $i/u-boot-sunxi-with-spl.bin $DESTDIR/uboot/boards/sun7i/$i
 done
 popd
 
@@ -93,12 +109,28 @@ for lcd in "" "-lcd7" "-lcd10"; do
         if [ ! -d $DESTDIR/uboot/boards/sun4i/$i ]; then
             mkdir -p $DESTDIR/uboot/boards/sun4i/$i
         fi
-        if [ ! -f sys_config/a10/${i,,}$lcd.fex ]; then
+        if [ ! -f sys_config/a10/$i$lcd.fex ]; then
             continue
         fi
-        cp -p sys_config/a10/${i,,}$lcd.fex $DESTDIR/uboot/boards/sun4i/$i
-        fex2bin sys_config/a10/${i,,}$lcd.fex \
+        cp -p sys_config/a10/$i$lcd.fex $DESTDIR/uboot/boards/sun4i/$i
+        fex2bin sys_config/a10/$i$lcd.fex \
             $DESTDIR/uboot/boards/sun4i/$i/script$lcd.bin
+    done
+    for i in $A13_BOARDS; do
+        if [ ! -f sys_config/a13/$i$lcd.fex ]; then
+            continue
+        fi
+        cp -p sys_config/a13/$i$lcd.fex $DESTDIR/uboot/boards/sun5i/$i
+        fex2bin sys_config/a13/$i$lcd.fex \
+            $DESTDIR/uboot/boards/sun5i/$i/script$lcd.bin
+    done
+    for i in $A20_BOARDS; do
+        if [ ! -f sys_config/a20/$i$lcd.fex ]; then
+            continue
+        fi
+        cp -p sys_config/a20/$i$lcd.fex $DESTDIR/uboot/boards/sun7i/$i
+        fex2bin sys_config/a20/$i$lcd.fex \
+            $DESTDIR/uboot/boards/sun7i/$i/script$lcd.bin
     done
 done
 popd
@@ -107,35 +139,55 @@ pushd sunxi-kernel-config
 [ -z "$NOCHECKOUT" ] && git checkout $KERNEL_CONFIG_TAG
 [ -z "$NOCLEAN" ] && git clean -dxf
 make VERSION=$KERNER_VER -f Makefile.config kernel-$KERNER_VER-armv7hl-sun4i.config
+make VERSION=$KERNER_VER -f Makefile.config kernel-$KERNER_VER-armv7hl-sun5i.config
+make VERSION=$KERNER_VER -f Makefile.config kernel-$KERNER_VER-armv7hl-sun7i.config
 popd
 
 pushd linux-sunxi
 [ -z "$NOCHECKOUT" ] && git checkout $KERNEL_TAG
 [ -z "$NOCLEAN" ] && git clean -dxf
-mkdir -p sun4i
+mkdir -p sun4i sun5i sun7i
 cp ../sunxi-kernel-config/kernel-$KERNER_VER-armv7hl-sun4i.config sun4i/.config
-make O=sun4i ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- CONFIG_DEBUG_SECTION_MISMATCH=y -j10 uImage modules
+cp ../sunxi-kernel-config/kernel-$KERNER_VER-armv7hl-sun5i.config sun5i/.config
+cp ../sunxi-kernel-config/kernel-$KERNER_VER-armv7hl-sun7i.config sun7i/.config
+make O=sun4i ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- CONFIG_DEBUG_SECTION_MISMATCH=y -j8 uImage modules
+make O=sun5i ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- CONFIG_DEBUG_SECTION_MISMATCH=y -j8 uImage modules
+make O=sun7i ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- CONFIG_DEBUG_SECTION_MISMATCH=y -j8 uImage modules
 
-cp sun4i/arch/arm/boot/uImage $DESTDIR/uboot/uImage
+cp sun4i/arch/arm/boot/uImage $DESTDIR/uboot/uImage.sun4i
+cp sun5i/arch/arm/boot/uImage $DESTDIR/uboot/uImage.sun5i
+cp sun7i/arch/arm/boot/uImage $DESTDIR/uboot/uImage.sun7i
 
 mkdir $DESTDIR/rootfs/usr
 make O=sun4i ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- CONFIG_DEBUG_SECTION_MISMATCH=y INSTALL_MOD_PATH=$DESTDIR/rootfs/usr modules_install
+make O=sun5i ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- CONFIG_DEBUG_SECTION_MISMATCH=y INSTALL_MOD_PATH=$DESTDIR/rootfs/usr modules_install
+make O=sun7i ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- CONFIG_DEBUG_SECTION_MISMATCH=y INSTALL_MOD_PATH=$DESTDIR/rootfs/usr modules_install
 find $DESTDIR/rootfs/usr/lib/modules -name "*.ko" -exec arm-linux-gnueabihf-strip --strip-debug '{}' \;
 
 mkdir $DESTDIR/uboot/scripts
 cp sun4i/.config $DESTDIR/uboot/scripts/kernel-$KERNER_VER-armv7hl-sun4i.config
+cp sun5i/.config $DESTDIR/uboot/scripts/kernel-$KERNER_VER-armv7hl-sun5i.config
+cp sun7i/.config $DESTDIR/uboot/scripts/kernel-$KERNER_VER-armv7hl-sun7i.config
 popd
 
 pushd sunxi-fedora-scripts
 [ -z "$NOCHECKOUT" ] && git checkout $SCRIPTS_TAG
 [ -z "$NOCLEAN" ] && git clean -dxf
-../u-boot-sunxi/A10_MID_1GB/tools/mkenvimage -s 131072 -o $DESTDIR/uboot/boards/uEnv-img.bin uEnv-full.txt
+../u-boot-sunxi/mele_a1000/tools/mkenvimage -s 131072 \
+  -o $DESTDIR/uboot/boards/uEnv-img.bin uEnv-full.txt
 mkimage -C none -A arm -T script -d boot.cmd $DESTDIR/uboot/boot.scr
-cp -p $DESTDIR/uboot/boards/sun4i/A10_MID_1GB/u-boot-sunxi-with-spl.bin $DESTDIR/uboot/boards/sun4i/meep1
 cp -p boot.cmd README select-board.sh $DESTDIR/uboot
 cp -p uEnv-boot.txt $DESTDIR/uboot/uEnv.txt
 cp -p build-boot-root.sh build-image.sh $DESTDIR/uboot/scripts
-
+# Add F-18 rootfs-resize (+ patch for no initrd + hack for rhbz#974631)
+mkdir -p $DESTDIR/rootfs/usr/sbin
+mkdir -p $DESTDIR/rootfs/usr/lib/systemd/system
+mkdir -p $DESTDIR/rootfs/etc/systemd/system/multi-user.target.wants
+cp -p rootfs-resize $DESTDIR/rootfs/usr/sbin
+cp -p rootfs-resize.service $DESTDIR/rootfs/usr/lib/systemd/system
+ln -s /usr/lib/systemd/system/rootfs-resize.service \
+  $DESTDIR/rootfs/etc/systemd/system/multi-user.target.wants/rootfs-resize.service
+touch $DESTDIR/rootfs/.rootfs-repartition
 # Add rc.local
 mkdir -p $DESTDIR/rootfs/etc/rc.d
 cp -p rc.local $DESTDIR/rootfs/etc/rc.d
@@ -149,7 +201,7 @@ tar --group=root --owner=root -czf $DESTDIR/uboot.tar.gz *
 popd
 
 pushd $DESTDIR/rootfs
-tar --group=root --owner=root -czf $DESTDIR/rootfs.tar.gz *
+tar --group=root --owner=root -czf $DESTDIR/rootfs.tar.gz .rootfs-repartition *
 popd
 
 echo "Successfully generated uboot.tar.gz and rootfs.tar.gz"
